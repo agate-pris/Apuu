2d68455078b1606390b71b15dcd9536b9d351c5f
agate-pris <agate-pris@users.noreply.github.com>
2020-12-06 08:16:35 +0900

    Add InstantiateWithOriginalPositionAndRotation

--- Scripts/Utility.cs
+++ Scripts/Utility.cs
@@ -1,3 +1,26 @@
+using UnityEngine;
+
 namespace AgatePris.Apuu {
-    public class Utility { }
+    public class Utility {
+        public static T InstantiateWithOriginalPositionAndRotation<T>(
+            in T original, in Vector3 position, in Quaternion rotation) where T : Component
+            => Object.Instantiate(
+                original,
+                position + (rotation * original.transform.localPosition),
+                rotation * original.transform.localRotation);
+        public static T InstantiateWithOriginalPositionAndRotation<T>(
+            in T original,
+            in Vector3 position,
+            in Quaternion rotation,
+            in Transform parent) where T : Component {
+            var matrix
+                = parent.localToWorldMatrix
+                * Matrix4x4.Rotate(Quaternion.Inverse(parent.rotation) * rotation);
+            return Object.Instantiate(
+                original,
+                position + matrix.MultiplyVector(original.transform.localPosition),
+                rotation * original.transform.localRotation,
+                parent);
+        }
+    }
 }
