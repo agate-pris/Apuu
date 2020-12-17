662b395f3254de7cb51fc9f50ab5e883e401fd82
agate-pris <agate-pris@users.noreply.github.com>
2020-12-17 23:13:59 +0900

    Add AnyOfBehavioursIsActiveAndEnabled

--- Scripts/Utility.cs
+++ Scripts/Utility.cs
@@ -91,6 +91,18 @@ namespace AgatePris.Apuu {
             }
             return true;
         }
+        public static bool AnyOfBehavioursIsActiveAndEnabled<T>(in T behaviours)
+            where T : IEnumerable<Behaviour> {
+            foreach (var i in behaviours) {
+                if (!i) {
+                    continue;
+                }
+                if (i.isActiveAndEnabled) {
+                    return true;
+                }
+            }
+            return false;
+        }
         public static T InstantiateWithOriginalPositionAndRotation<T>(
             in T original, in Vector3 position, in Quaternion rotation) where T : Component
             => Object.Instantiate(
