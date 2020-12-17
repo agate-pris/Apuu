78439197528058ca776860b501b5b1bcadd45891
agate-pris <agate-pris@users.noreply.github.com>
2020-12-18 03:11:56 +0900

    Add TestAllOfObjectsAreDestroyed

--- Editor/UtilityTest.cs
+++ Editor/UtilityTest.cs
@@ -1,4 +1,20 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
 namespace AgatePris.Apuu {
     public class UtilityTest {
+        [Test]
+        public void TestAllOfObjectsAreDestroyed() {
+            var gameObject = new GameObject();
+            var behaviour = gameObject.AddComponent<EventTrigger>();
+            var l = new List<EventTrigger>();
+            Assert.IsTrue(Utility.AllOfObjectsAreDestroyed(l));
+            l.Add(behaviour);
+            Assert.IsFalse(Utility.AllOfObjectsAreDestroyed(l));
+            Object.DestroyImmediate(behaviour);
+            Assert.IsTrue(Utility.AllOfObjectsAreDestroyed(l));
+        }
     }
 }
