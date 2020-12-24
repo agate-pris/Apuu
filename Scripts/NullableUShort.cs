cad296f5b4f92fcc97342f981b63dc359beaaeff
agate-pris <agate-pris@users.noreply.github.com>
2020-12-25 02:33:52 +0900

    Add the class NullableUShort

---
+++ Scripts/NullableUShort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace AgatePris.Apuu {
+    [Serializable]
+    public struct NullableUShort {
+        [SerializeField] bool hasValue;
+        [SerializeField] ushort value;
+
+        public bool HasValue => hasValue;
+        public ushort Value => !hasValue
+            ? throw new InvalidOperationException("Nullable object must have a value.") : value;
+
+        public NullableUShort(ushort value) {
+            hasValue = true;
+            this.value = value;
+        }
+        public override bool Equals(object other)
+            => !hasValue ? other is null : value.Equals(other);
+        public override int GetHashCode() => !hasValue ? 0 : value.GetHashCode();
+        public ushort GetValueOrDefault() => !hasValue ? default : value;
+        public ushort GetValueOrDefault(ushort defaultValue) => !hasValue ? defaultValue : value;
+        public override string ToString() => !hasValue ? "" : value.ToString();
+
+        public static implicit operator NullableUShort(ushort value) => new NullableUShort(value);
+        public static explicit operator ushort(NullableUShort value) => value.Value;
+    }
+}
