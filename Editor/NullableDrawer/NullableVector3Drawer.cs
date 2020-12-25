d28d66043cdf6e001f25e8118867cbef540c2aea
agate-pris <agate-pris@users.noreply.github.com>
2020-12-26 01:05:03 +0900

    Add the class NullableVector3Drawer

---
+++ Editor/NullableDrawer/NullableVector3Drawer.cs
@@ -0,0 +1,4 @@
+﻿namespace AgatePris.Apuu.NullableDrawer {
+    [UnityEditor.CustomPropertyDrawer(typeof(NullableVector3))]
+    public class NullableVector3Drawer : OverlapWrapNullableDrawer { }
+}
